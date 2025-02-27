6a45fc97aee142eb64d707542a491aca4b7ac183
Michael Morby-Smith <michaelmorbysmith@gmail.com>
2025-02-27 17:22:47 -0600

    updates

--- app.py
+++ app.py
@@ -36,4 +36,9 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f"Running the experiment of {number_of_trials} trials")
-    mean = toss_coint(number_of_trials)
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([st.session_state['experiment_no'], pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]], columns=['no', 'iterations', 'mean'])], axis=0)
+    st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
