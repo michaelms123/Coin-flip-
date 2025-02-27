09c6f96e9bd8dea9b1b9a712e2f23f926242cec7
Michael Morby-Smith <michaelmorbysmith@gmail.com>
2025-02-27 14:53:58 -0600

    updates

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import steamlit as st 
+import steamlit as st  
 
 st.header('Tossing a Coin')
 
-st.write('It is not a functional application yet, under construction.')
+number of trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button: 
+    st.write(f'Running the experiment of {number_of_trials} trials')
+
+
+st.write('It is not a functional application yet, under construction.'
