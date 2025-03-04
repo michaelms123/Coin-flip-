0537e53c7333962bcf52a05ed7ed60c2ceeea975
Michael Morby-Smith <michaelmorbysmith@gmail.com>
2025-03-04 14:27:45 -0600

    final commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import streamlit as st   
-import scipy.stats
-import time
-import pandas as pd 
 
+import pandas as pd
+import scipy.stats
+import streamlit as st
+import time
+
+# these are stateful variables which are preserved as Streamlit reruns this script
 if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0 
+    st.session_state['experiment_no'] = 0
 
 if 'df_experiment_results' not in st.session_state:
     st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
@@ -21,24 +23,32 @@ def toss_coin(n):
     outcome_no = 0
     outcome_1_count = 0
 
-    for r in trial_outcomes: 
+    for r in trial_outcomes:
         outcome_no +=1
-        if r == 1: 
+        if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
         time.sleep(0.05)
 
-    return mean 
+    return mean
 
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
-    st.write(f"Running the experiment of {number_of_trials} trials")
+    st.write(f'Running the experiment of {number_of_trials} trials.')
     st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([st.session_state['experiment_no'], pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]], columns=['no', 'iterations', 'mean'])], axis=0)
-    st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
